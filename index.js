bdd7706f354d2c630828212e2fbcfaaed3192894
chukwus618 <chukwus618@gmail.com>
2025-07-22 18:12:35 +0100

    Add javascript syntax and logic to enable popup modal when 'See Project' button is clicked.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const works = [
   {
+    index: 0,
     nameMobile: 'Tonic',
     nameDesktop: 'Tonic',
     mobileDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
@@ -8,14 +9,16 @@ const works = [
     featuredImageDesktop: 'icons/snapshoot5.png',
     clientMobile: 'CANOPY',
     roleMobile: 'Back End Dev',
+    clientDesktop: 'CANOPY',
+    roleDesktop: 'Back End Dev',
     yearMobile: '2015',
     yearDesktop: '2015',
-    tagMobile: ['HTML', 'CSS', 'Javascript'],
-    tagDesktop: ['HTML', 'CSS', 'Javascript'],
+    tag: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
     seeProjectBtn: '',
   },
 
   {
+    index: 1,
     nameMobile: 'Multi-Post Stories',
     nameDesktop: 'Multi-Post Stories',
     mobileDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
@@ -28,12 +31,12 @@ const works = [
     roleDesktop: 'Full Stack Dev',
     yearMobile: '2015',
     yearDesktop: '2015',
-    tagMobile: ['HTML', 'CSS', 'Javascript'],
-    tagDesktop: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
+    tag: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
     seeProjectBtn: '',
   },
 
   {
+    index: 2,
     nameMobile: 'Tonic',
     nameDesktop: 'Facebook 360',
     mobileDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
@@ -46,12 +49,12 @@ const works = [
     roleDesktop: 'Full Stack Dev',
     yearMobile: '2015',
     yearDesktop: '2015',
-    tagMobile: ['HTML', 'CSS', 'Javascript'],
-    tagDesktop: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
+    tag: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
     seeProjectBtn: '',
   },
 
   {
+    index: 3,
     nameMobile: 'Multi-Post Stories',
     nameDesktop: 'Uber Navigation',
     mobileDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
@@ -64,8 +67,7 @@ const works = [
     roleDesktop: 'Lead Developer',
     yearMobile: '2015',
     yearDesktop: '2018',
-    tagMobile: ['HTML', 'CSS', 'Javascript'],
-    tagDesktop: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
+    tag: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
     seeProjectBtn: '',
   },
 ];
@@ -75,18 +77,21 @@ window.addEventListener('DOMContentLoaded', () => {
   const closeIcon = document.getElementById('close-icon');
   const navBar = document.getElementById('nav-bar');
   const overLay = document.getElementById('overlay');
-  const shape = document.getElementById('shape');
   const bodyElement = document.getElementsByTagName('body')[0];
-  const contactSection = document.getElementById('contact-info');
-  const popup = document.getElementById('details-popup');
+  const popup = document.createElement('article');
+  popup.id = 'details-popup';
+  popup.style.display = 'none';
+  document.body.appendChild(popup);
+
   const projectContainer = document.querySelector('.work-section');
 
   menuIcon.addEventListener('click', () => {
     navBar.style.display = 'flex';
     navBar.classList.add('active');
     overLay.style.display = 'block';
+    overLay.classList.add('overlay-menu');
+    overLay.classList.remove('overlay-popup');
     closeIcon.style.display = 'block';
-    overLay.appendChild(shape);
     bodyElement.style.overflow = 'hidden';
   });
 
@@ -95,8 +100,8 @@ window.addEventListener('DOMContentLoaded', () => {
     menuIcon.style.display = 'block';
     closeIcon.style.display = 'none';
     overLay.style.display = 'none';
-    contactSection.appendChild(shape);
     popup.style.display = 'none';
+    bodyElement.style.overflow = 'auto';
   });
 
   navBar.addEventListener('click', () => {
@@ -107,9 +112,9 @@ window.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('resize', () => console.log(window.innerWidth));
   };
 
-  console.log(checKInnerWidth());
+  checKInnerWidth();
 
-  const fetchProjectCard = ((works) => (
+  const fetchProjectCard = (works, index) => (
     `<section class="works">
       <img class="snapshoot hide-desk" src="${works.featuredImageMobile}" alt="${works.nameMobile} snapshot"/>
       <img class="snapshoot hide-mobile" src="${works.featuredImageDesktop}" alt="${works.nameDesktop} snapshot"/>
@@ -119,67 +124,117 @@ window.addEventListener('DOMContentLoaded', () => {
         <ul>
           <li class="caption1 hide-desk">${works.clientMobile}</li>
           <li class="caption1 hide-mobile">${works.clientDesktop}</li>
-          <div id="circle"></div>
+          <div class="circle"></div>
           <li class="hide-desk"><span>${works.roleMobile}</span></li>
           <li class="hide-mobile"><span>${works.roleDesktop}</span></li>
-          <div id="circle"></div>
+          <div class="circle"></div>
           <li class="hide-desk"><span>${works.yearMobile}</span></li>
           <li class="hide-mobile"><span>${works.yearMobile}</span></li>
         </ul>
         <p class="text3 hide-desk">${works.mobileDescription}</p>
         <p class="text3 hide-mobile">${works.desktopDescription}</p>
         <ul>
-          ${works.tagMobile.map((tag) => `<li><p class="tag hide-desk">${tag}</p></li>`).join('')}
-          ${works.tagDesktop.map((tag) => `<li><p class="tag hide-mobile">${tag}</p></li>`).join('')}
+         <li class="tag1">
+           <p class="tag">${works.tag[0]}</p>
+         </li>
+         <li class="tag4">
+           <p class="notmobile tag" id="tag4">${works.tag[1]}</p>
+         </li>
+         <li class="tag2">
+           <p class="tag">${works.tag[2]}</p>
+         </li>
+         <li class="tag3">
+           <p class="tag">${works.tag[3]}</p>
+         </li>
         </ul>
-        <button class="project-button" type="button">See Project</button>
+        <button class="project-button" type="button" data-index="${index}">See Project</button>
       </div>
     </section>`
-  ));
+  );
 
-  projectContainer.innerHTML = works.map((work) => fetchProjectCard(work));
+  projectContainer.innerHTML = works.map((works, index) => fetchProjectCard(works, index)).join('');
 
-  popup.innerHTML = 'Right here';
+  // const popupButtons = document.querySelectorAll('.project-button');
 
-  const displayPop = ((index) => {
-    return (
-    `<section id="popup">
-        <img src="${works[index].featuredImageMobile}" id="close-icon" alt="close-icon"/>
-        <h3 class="header-desktop">Tonic</h3>
-          <ul>
-            <li class="caption1 hide-desk">${works[index].clientMobile}</li>
-            <li class="caption1 hide-mobile">${works[index].clientDesktop}</li>
-            <div id="circle"></div>
-            <li class="hide-desk"><span>${works[index].roleMobile}</span></li>
-            <li class="hide-mobile"><span>${works[index].roleDesktop}</span></li>
-            <div id="circle"></div>
-            <li class="hide-desk"><span>${works[index].yearMobile}</span></li>
-            <li class="hide-mobile"><span>${works[index].yearMobile}</span></li>
-        </ul>
-          <img class="snapshoot hide-desk" src="${works[index].featuredImageMobile}" alt="${works[index].nameMobile} snapshot"/>
-          <img class="snapshoot hide-mobile" src="${works[index].featuredImageDesktop}" alt="${works[index].nameDesktop} snapshot"/>
-        <p class="text3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent</p>
-        <ul>
-            ${works[index].tagMobile.map((tag) => `<li><p class="tag hide-desk">${tag}</p></li>`).join('')}
-            ${works[index].tagDesktop.map((tag) => `<li><p class="tag hide-mobile">${tag}</p></li>`).join('')}
-          </ul>
-        <div id="divider"></div>
-        <button type="button" id="live-button">See live</button>
-        <button type="button" id="source-button">See source</button>
-      </section>`);
-  });
-
-  // const fetchPopup.forEach((works) => {
-
+  // popupButtons.forEach((button) => {
+  //   button.addEventListener('click', (e) => {
+  //     const index = e.currentTarget.getAttribute('data-index');
+  //     showPopup(index);
+  //   });
   // });
 
-  const popupButtons = document.querySelectorAll('.project-button');
-  popupButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-      console.log('Button clicked!', index);
-      overLay.style.display = 'block';
-      closeIcon.style.display = 'block';
-      shape.style.zIndex = 1000;
+  const displayPop = (index) => (
+    `<section class="popup-content">
+      <section class="popup-header">
+       <h3 class="header-desktop hide-desk">${works[index].nameMobile}</h3>
+       <h3 class="header-desktop hide-mobile">${works[index].nameDesktop}</h3>
+       <img src="icons/popup-close-icon.png" alt="Close" class="popup-close-icon"/>
+      </section>
+      <ul>
+        <li class="caption1 hide-desk">${works[index].clientMobile}</li>
+        <li class="caption1 hide-mobile">${works[index].clientDesktop}</li>
+        <div class="circle"></div>
+        <li class="hide-desk"><span>${works[index].roleMobile}</span></li>
+        <li class="hide-mobile"><span>${works[index].roleDesktop}</span></li>
+        <div class="circle"></div>
+        <li class="hide-desk"><span>${works[index].yearMobile}</span></li>
+        <li class="hide-mobile"><span>${works[index].yearDesktop}</span></li>
+      </ul>
+      <img class="snapshoot hide-desk" src="${works[index].featuredImageMobile}" alt="${works[index].nameMobile} snapshot"/>
+      <img class="snapshoot hide-mobile" src="${works[index].featuredImageDesktop}" alt="${works[index].nameDesktop} snapshot"/>
+      <section class="desktop-popup-content">
+       <section class="project-description-modal">
+         <p class="text3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent</p>
+         <p class="text3 hide-mobile">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.</p>
+       </section>
+       <aside>
+         <ul>
+           <li class="tag1">
+             <p class="tag">${works[index].tag[0]}</p>
+           </li>
+           <li class="tag4">
+             <p class="notmobile tag" id="tag4">${works[index].tag[1]}</p>
+           </li>
+           <li class="tag2">
+             <p class="tag">${works[index].tag[2]}</p>
+           </li>
+           <li class="tag3">
+             <p class="tag">${works[index].tag[3]}</p>
+           </li>
+         </ul>
+         <div id="divider"></div>
+         <button type="button" id="live-button">See live</button>
+         <button type="button" id="source-button">See source</button>
+       </aside>
+      </section>
+    </section>`
+  );
+  const showPopup = (index) => {
+    popup.innerHTML = displayPop(index);
+    const overLay = document.getElementById('overlay');
+    overLay.classList.remove('overlay-menu');
+    overLay.classList.add('overlay-popup');
+    overLay.style.display = 'block';
+    popup.style.display = 'block';
+    bodyElement.style.overflow = 'hidden';
+
+    const popupCloseIcon = popup.querySelector('.popup-close-icon');
+    popupCloseIcon.addEventListener('click', () => {
+      popup.style.display = 'none';
+      overLay.style.display = 'none';
+      overLay.classList.remove('overlay-menu', 'overlay-popup');
+      bodyElement.style.overflow = 'auto';
     });
+    console.log('here', index);
+    console.log('Popup content HTML:', popup.innerHTML);
+  };
+  projectContainer.addEventListener('click', (event) => {
+    // Check if the clicked element has the 'project-button' class
+    if (event.target && event.target.classList.contains('project-button')) {
+      const index = event.target.getAttribute('data-index');
+      showPopup(index);
+    }
   });
+  console.log('Popup element:', popup);
+  console.log('Popup display style:', popup.style.display);
 });
